a7c83c9a27d8b5b3cc60fc1da263be1811bd7e19
Srikant14 <srikant.sg14@gmail.com>
2017-05-11 21:15:44 +0530

    joyal edit

--- app/src/main/java/co/tripzana/tripzana/register_activity.java
+++ app/src/main/java/co/tripzana/tripzana/register_activity.java
@@ -1,13 +1,57 @@
 package co.tripzana.tripzana;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class register_activity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class register_activity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener{
+
+
+    private LinearLayout Prof_section;
+    private Button SignOut;
+    private SignInButton SignIn;
+    private TextView Name,Email;
+    private ImageView Prof_pic;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_CODE = 9001;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_activity);
+        Prof_section = (LinearLayout)findViewById(R.id.user_prof);
+        SignOut = (Button)findViewById(R.id.bn_logout);
+        SignIn = (SignInButton)findViewById(R.id.bn_login);
+        Name = (TextView)findViewById(R.id.name);
+        Email = (TextView)findViewById(R.id.email);
+        Prof_pic = (ImageView)findViewById(R.id.prof_pic);
+        SignIn.setOnClickListener(this);
+        SignOut.setOnClickListener(this);
+        Prof_section.setVisibility(View.GONE);
+
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
