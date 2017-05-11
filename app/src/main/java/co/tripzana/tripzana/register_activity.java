b533c5660a0c29c1a999dfc552e30de17e3db701
Srikant14 <srikant.sg14@gmail.com>
2017-05-12 00:33:14 +0530

    joyal edit

--- app/src/main/java/co/tripzana/tripzana/register_activity.java
+++ app/src/main/java/co/tripzana/tripzana/register_activity.java
@@ -1,5 +1,6 @@
 package co.tripzana.tripzana;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,9 +10,16 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
 public class register_activity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener{
 
@@ -22,7 +30,8 @@ public class register_activity extends AppCompatActivity implements View.OnClick
     private TextView Name,Email;
     private ImageView Prof_pic;
     private GoogleApiClient googleApiClient;
-    private static final int REQ_CODE = 9001;
+    private static final int REQ_CODE =9001;
+    public String name = "joyal";
 
 
     @Override
@@ -38,20 +47,96 @@ public class register_activity extends AppCompatActivity implements View.OnClick
         SignIn.setOnClickListener(this);
         SignOut.setOnClickListener(this);
         Prof_section.setVisibility(View.GONE);
-
-
-
-
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build();
 
     }
 
     @Override
     public void onClick(View v) {
 
+        switch(v.getId())
+        {
+            case R.id.bn_login :
+                signIn();
+                break;
+            case R.id.bn_logout:
+                signOut();
+                break;
+
+        }
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    private void signIn()
+    {
+
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent,REQ_CODE);
+    }
+
+    private void signOut()
+    {
+
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                updateUI(false);
+            }
+        });
+    }
+
+    private void handleResult(GoogleSignInResult result)
+    {
+
+        if (result.isSuccess())
+        {
+            GoogleSignInAccount account = result.getSignInAccount();
+             name = account.getDisplayName(); //user name
+            String email = account.getEmail();  //user email
+            String img_url = account.getPhotoUrl().toString(); //profile pic
+            Name.setText(name);
+            Email.setText(email);
+            if(img_url != null) {
+                Glide.with(this).load(img_url).into(Prof_pic);
+            }
+            updateUI(true);
+        }
+        else
+        {
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean isLogin)
+    {
+
+        if(isLogin)
+        {
+            Prof_section.setVisibility(View.VISIBLE);
+            SignIn.setVisibility(View.GONE);
+        }
+        else
+        {
+            Prof_section.setVisibility(View.GONE);
+            SignIn.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode,data);
+
+        if (requestCode==REQ_CODE)
+        {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+        }
     }
 }
